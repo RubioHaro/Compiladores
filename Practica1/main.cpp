14734637317c3fe1c9767f71a948ace37bbe1d45
Rodrigo Rubio Haro <chavo0022009@gmail.com>
2021-08-16 18:22:29 -0500

    1.Prototype

--- Practica1/main.cpp
+++ Practica1/main.cpp
@@ -1,5 +1,84 @@
+#include <iostream>
+#include <string>
+
 int main(int argc, char const *argv[])
 {
     /* code */
     return 0;
 }
+
+class finiteAutomaton
+{
+private:
+    std::string Q[10];
+    char E[100];
+    std::string q;
+    std::string F[10];
+public:
+    void move(/* args */);
+    void acceptance(/* args */);
+    finiteAutomaton(/* args */);
+    ~finiteAutomaton();
+};
+
+finiteAutomaton::finiteAutomaton(/* args */)
+{
+}
+
+
+void finiteAutomaton::move(/* args */)
+{
+}
+
+void finiteAutomaton::acceptance(/* args */)
+{
+}
+
+finiteAutomaton::~finiteAutomaton()
+{
+}
+
+class NFA: public finiteAutomaton
+{
+private:
+    /* data */
+public:
+    void transitionFunction(/* args */);
+    NFA(/* args */);
+    ~NFA();
+};
+
+NFA::NFA(/* args */)
+{
+}
+
+NFA::~NFA()
+{
+}
+void NFA::transitionFunction(/* args */)
+{
+}
+
+class DFA: public finiteAutomaton
+{
+private:
+    
+    /* data */
+public:
+
+    void transitionFunction(/* args */);
+    DFA(/* args */);
+    ~DFA();
+};
+
+DFA::DFA(/* args */)
+{
+}
+
+void DFA::transitionFunction(/* args */)
+{
+}
+DFA::~DFA()
+{
+}
+
